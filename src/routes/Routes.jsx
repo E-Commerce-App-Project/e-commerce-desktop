3b12ff10b9dde071486e877ba66591faf5299b90
ATiarK <97088690+ATiarK@users.noreply.github.com>
2022-04-07 12:44:24 +0700

    add route navigation

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from '../pages/App';
 import Detail from '../pages/Detail';
+import ShoppingCart from '../pages/ShoppingCart';
+import Transaction from '../pages/Transaction';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
 
 function Routing() {
 	return (
@@ -9,6 +13,10 @@ function Routing() {
 			<Routes>
 				<Route path='/' element={<App />} />
 				<Route path='/detail' element={<Detail />} />
+				<Route path='/login' element={<Login />} />
+				<Route path='/register' element={<Register />} />
+				<Route path='/transaction' element={<Transaction />} />
+				<Route path='/cart' element={<ShoppingCart />} />
 			</Routes>
 		</BrowserRouter>
 	);
