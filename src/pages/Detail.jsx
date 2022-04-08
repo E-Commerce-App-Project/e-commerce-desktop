08c61f05a985959e34bdacc81e3036e630f0b05f
ATiarK <97088690+ATiarK@users.noreply.github.com>
2022-04-08 12:54:49 +0700

    update path API detail

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -23,7 +23,6 @@ export default function Detail() {
 			)
 			.then((response) => {
 				setProducts(response.data.data);
-				console.log(response.data.data);
 			})
 			.catch((err) => {
 				console.log(err);
