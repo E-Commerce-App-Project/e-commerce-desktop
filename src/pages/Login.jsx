028de093acc1eb600def6063fa926171834f29e2
ATiarK <97088690+ATiarK@users.noreply.github.com>
2022-04-08 09:42:19 +0700

    update login & register feature

---
+++ src/pages/Login.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import { Button } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+export default function Login() {
+	const navigate = useNavigate();
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		await axios
+			.post(`https://web-app-zgunz42.cloud.okteto.net/api/v1/login`, {
+				email: email,
+				password: password,
+			})
+			.then((response) => {
+				const { data, status } = response.data;
+				localStorage.setItem('token', data.token);
+				if (status === 'success') {
+					navigate('/');
+				} else {
+					alert('Login failed');
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	return (
+		<div className='bg-info overflow-auto' style={{ height: 100 + 'vh' }}>
+			<div
+				className='container bg-light ms-auto my-5 py-lg-3 px-lg-5'
+				style={{ borderRadius: 1 + 'em' }}>
+				<h1>LOGIN</h1>
+				<Form>
+					<Form.Group className='mb-3' controlId='formBasicEmail'>
+						<Form.Label>Email</Form.Label>
+						<Form.Control
+							type='email'
+							placeholder='Enter your email'
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+					</Form.Group>
+					<Form.Group className='mb-3' controlId='formBasicPassword'>
+						<Form.Label>Password</Form.Label>
+						<Form.Control
+							type='password'
+							aria-describedby='passwordHelpBlock'
+							placeholder='Enter your password'
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+					</Form.Group>
+					<Button
+						variant='primary'
+						type='submit'
+						onClick={handleSubmit}>
+						Log In
+					</Button>
+					<br />
+					<Link to='/register'>
+						<Form.Text className='text-muted'>
+							Do not have an account? Click Register
+						</Form.Text>
+					</Link>
+				</Form>
+			</div>
+		</div>
+	);
+}
---
+++ src/pages/Register.jsx
@@ -0,0 +1,135 @@
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { Button, Form, Row, Col, FormLabel } from 'react-bootstrap';
+import Layout from '../components/Layout';
+
+export default function Register() {
+	const navigate = useNavigate();
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [phone, setPhone] = useState('');
+	const [address, setAddress] = useState('');
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		await axios
+			.post(`https://web-app-zgunz42.cloud.okteto.net/api/v1/users`, {
+				name: name,
+				email: email,
+				password: password,
+				phone_number: phone,
+				address: address,
+			})
+			.then((response) => {
+				const { status } = response.data;
+				if (status === 'success') {
+					navigate('/login');
+				} else {
+					alert('Register failed');
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	return (
+		<Layout>
+			<div
+				className='container bg-light my-5 py-lg-3 px-lg-5'
+				style={{ borderRadius: 1 + 'em' }}>
+				<Row>
+					<Col>
+						<Form>
+							<Form.Group
+								className='mb-3'
+								controlId='formBasicName'>
+								<FormLabel>
+									Name <span>*</span>
+								</FormLabel>
+								<Form.Control
+									type='text'
+									placeholder='Enter your name'
+									value={name}
+									onChange={(e) => setName(e.target.value)}
+								/>
+							</Form.Group>
+							<Form.Group
+								className='mb-3'
+								controlId='formBasicEmail'>
+								<FormLabel>
+									Email <span>*</span>
+								</FormLabel>
+								<Form.Control
+									type='email'
+									placeholder='Enter your email'
+									value={email}
+									onChange={(e) => setEmail(e.target.value)}
+								/>
+							</Form.Group>
+							<Form.Group
+								className='mb-3'
+								controlId='formBasicPassword'>
+								<FormLabel>
+									Password <span>*</span>
+								</FormLabel>
+								<Form.Control
+									type='password'
+									value={password}
+									aria-describedby='passwordHelpBlock'
+									placeholder='Enter your password'
+									onChange={(e) =>
+										setPassword(e.target.value)
+									}
+								/>
+							</Form.Group>
+							<Form.Group
+								className='mb-3'
+								controlId='formBasicPhoneNumber'>
+								<FormLabel>
+									Phone Number <span>*</span>
+								</FormLabel>
+								<Form.Control
+									type='number'
+									placeholder='Enter your Phone Number'
+									value={phone}
+									onChange={(e) => setPhone(e.target.value)}
+								/>
+							</Form.Group>
+							<br />
+							<Form.Text className='text-muted'>
+								<span>*</span> Required
+							</Form.Text>
+						</Form>
+					</Col>
+					<Col>
+						<Form>
+							<Form.Group
+								className='mb-3'
+								controlId='formBasicAddress'>
+								<FormLabel>
+									Address <span>*</span>
+								</FormLabel>
+								<Form.Control
+									as='textarea'
+									placeholder='Enter your address'
+									style={{ height: '300px' }}
+									value={address}
+									onChange={(e) => setAddress(e.target.value)}
+								/>
+							</Form.Group>
+							<Button
+								variant='primary'
+								type='submit'
+								onClick={handleSubmit}>
+								Register
+							</Button>
+						</Form>
+					</Col>
+				</Row>
+			</div>
+		</Layout>
+	);
+}
