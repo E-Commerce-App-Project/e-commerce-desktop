1d0e1ecdaa51ca204430002c5a8611c1a66d885c
ATiarK <97088690+ATiarK@users.noreply.github.com>
2022-04-09 20:49:06 +0700

    fixing count Total

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Form, FormCheck } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import { WideCard } from '../components/Card';
 import { Link, useNavigate } from 'react-router-dom';
 
@@ -10,8 +10,14 @@ function ShoppingCart() {
 	const navigate = useNavigate();
 	const shoppingCart = JSON.parse(localStorage.getItem('cart'));
 
-	const total = shoppingCart.price;
-	console.log(total);
+	function countTotal() {
+		let total = 0;
+		for (let i = 0; i < shoppingCart.length; i++) {
+			total += parseInt(shoppingCart[i].price);
+		}
+		return total;
+	}
+
 	return (
 		<div>
 			<Layout>
@@ -20,29 +26,6 @@ function ShoppingCart() {
 						className='container bg-light my-5 py-lg-3 px-lg-5'
 						style={{ borderRadius: 1 + 'em' }}>
 						{/* #1 Choose Address */}
-						<Form className='fw-bold d-flex mx-3 mb-2'>
-							<h6 className='fw-bold my-auto p-0'>
-								Send to Address:{' '}
-							</h6>
-							{['radio'].map((type) => (
-								<div key={`inline-${type}`} className='mx-5'>
-									<FormCheck
-										inline
-										label='My Address'
-										name='address'
-										type={type}
-										id={'myaddress'}
-									/>
-									<FormCheck
-										inline
-										label='Different Address'
-										name='address'
-										type={type}
-										id={'newaddress'}
-									/>
-								</div>
-							))}
-						</Form>
 
 						{/* #2 Selected Product */}
 						<div className='row'>
@@ -80,7 +63,9 @@ function ShoppingCart() {
 									<h5 className='px-5 py-0 my-0 fw-bold'>
 										TOTAL
 									</h5>
-									<h5 className='px-5 my-0 py-0'>{}</h5>
+									<h5 className='px-5 my-0 py-0'>
+										Rp. {countTotal().toLocaleString()}
+									</h5>
 								</div>
 								<div className='m-0 p-0'>
 									<Link to='/transaction'>
