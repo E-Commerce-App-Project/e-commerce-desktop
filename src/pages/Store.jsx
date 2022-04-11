c3198efb81779339d71c6ffa054c5958ea1abd42
ATiarK <97088690+ATiarK@users.noreply.github.com>
2022-04-11 12:43:21 +0700

    changed local & add input for edit product

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,88 +1,133 @@
-import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import Layout from '../components/Layout';
 import { SmallCard } from '../components/Card';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
+import Swal from 'sweetalert2';
 
 export default function Store() {
 	document.title = 'My Store';
 	const [products, setProducts] = useState([]);
-	const [skeleton] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-	const [isReady, setIsReady] = useState(false);
+	const navigate = useNavigate();
+	const [name, setName] = useState('');
+	const [price, setPrice] = useState(0);
+	const [stock, setStock] = useState(0);
+	const [image, setImage] = useState('');
 
-	useEffect(() => {
-		fetchProducts();
-	}, []);
-
-	const fetchProducts = async () => {
-		axios
-			.get(`https://web-app-zgunz42.cloud.okteto.net/api/v1/products`)
-			.then((response) => {
-				setProducts(response.data.data);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	};
-
-	const deleteProduct = async (id) => {
-		await axios
-			.delete(
-				`https://web-app-zgunz42.cloud.okteto.net/api/v1/products/${id}`
-			)
-			.then((res) => {
-				console.log(res);
-			})
-			.catch((err) => {
-				console.log(err);
-			})
-			.finally(() => setIsReady(true));
-	};
+	// If get data from localStorage
+	const userProducts = JSON.parse(localStorage.getItem('user-product'));
 
 	return (
 		<div>
 			<Layout>
 				<main>
-					<h2 className='text-center my-3 fw-bold'>My Store</h2>
+					<h2 className='text-center mb-1 mt-3 fw-bold pb-3'>
+						My Store
+					</h2>
 					<Link to='/new-product'>
-						<Button variant='success float-end' className='my-3'>
+						<Button
+							variant='success'
+							className='my-1 justify-content-center'>
+							<i className='fa-solid fa-circle-plus me-2'></i>
 							Add Product
 						</Button>
 					</Link>
-					<section
-						id='listProduct'
-						className='row justify-content-center my-2'>
-						{isReady
-							? skeleton.map((skeleton) => {
-									return (
-										<SmallCard
-											key={skeleton}
-											id={skeleton}
-											product='Product Name'
-											price='$0.00'
-											img='https://via.placeholder.com/150'
-											stock='0'
-											label='Remove'
-											click={deleteProduct}
-										/>
-									);
-							  })
-							: products.map((product) => {
-									return (
-										<SmallCard
-											key={product.id}
-											id={product.id}
-											product={product.name}
-											price={product.price}
-											img={product.image}
-											stock={product.stock}
-											label='Remove'
-											click={deleteProduct}
-										/>
-									);
-							  })}
-					</section>
+					<div className='container'>
+						<section
+							id='listProduct'
+							className='row justify-content-center my-2 border-top border-5'>
+							{userProducts.map((item, index) => {
+								return (
+									<SmallCard
+										key={index}
+										img={item.image}
+										product={item.product}
+										edit={() => {
+											setName(item.product);
+											setPrice(item.price);
+											setStock(item.stock);
+											setImage(item.image);
+											Swal.fire({
+												title: 'Edit Product',
+												html: `
+													<form>
+														<div className='form-group text-end justify-content-end'>
+														<br /><label>Product Name</label>
+															<br />
+															<input type='text' className='form-control' value=${item.product} />
+															</div>
+															<div className='form-group'>
+															<br /><label>Image</label>
+															<br />
+															<input type='text' className='form-control' value=${item.image} />
+															</div>
+															<div className='form-group'>
+															<br /><label>Price</label>
+															<br />
+															<input type='text' className='form-control' value=${item.price} />
+															</div>
+															<div className='form-group'>
+															<br />
+															<label>Stock</label>
+															<br />
+															<input type='text' className='form-control' value=${item.stock} />
+															</div>
+														</form>
+														`,
+												showCancelButton: true,
+												confirmButtonText: 'Edit',
+												confirmButtonColor: '#00a8ff',
+												cancelButtonText: 'Cancel',
+												showLoaderOnConfirm: true,
+											});
+										}}
+										price={parseInt(
+											item.price
+										).toLocaleString()}
+										stock={item.stock}
+										label={'Remove'}
+										color={'danger'}
+										click={() => {
+											Swal.fire({
+												title: 'Are you sure?',
+												text: "You won't be able to revert this!",
+												icon: 'warning',
+												showCancelButton: true,
+												confirmButtonColor: '#3085d6',
+												cancelButtonColor: '#d33',
+												confirmButtonText:
+													'Yes, delete it!',
+											}).then((result) => {
+												if (result.value) {
+													Swal.fire({
+														title: 'Deleted!',
+														text: 'Your file has been deleted.',
+														icon: 'success',
+														showConfirmButton: false,
+														confirmButtonColor:
+															'#3085d6',
+														timer: 1500,
+													});
+													let getLocal = JSON.parse(
+														localStorage.getItem(
+															'user-product'
+														)
+													);
+													getLocal.splice(index, 1);
+													localStorage.setItem(
+														'user-product',
+														JSON.stringify(getLocal)
+													);
+													setProducts(getLocal);
+													navigate('/store');
+												}
+											});
+										}}
+									/>
+								);
+							})}
+						</section>
+					</div>
 				</main>
 			</Layout>
 		</div>
